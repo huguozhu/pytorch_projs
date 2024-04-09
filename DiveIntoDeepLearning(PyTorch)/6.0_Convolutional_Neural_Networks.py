7f0b5ef02f678e29fc79f076a41820a35f8f2432
ted.hu <ted.hu@zoom.us>
2024-04-10 01:19:23 +0800

    4.10 early

--- DiveIntoDeepLearning(PyTorch)/6.0_Convolutional_Neural_Networks.py
+++ DiveIntoDeepLearning(PyTorch)/6.0_Convolutional_Neural_Networks.py
@@ -154,8 +154,43 @@ def v64_Multiple_Input_Output_Channel() :
 
 
 # ========== 6.5 汇聚层（Pooling）  ========== 
+# 目的：
+#   1) 降低卷积层对位置的敏感性
+#   2) 降低对空间采样表示的敏感性
+# 种类：
+#   1) 最大汇聚层
+#   2) 平均汇聚层
+def v65_Pooling() :
+    def pool2d(X, pool_size, mode='max'):
+        p_h, p_w = pool_size
+        Y = torch.zeros((X.shape[0] - p_h + 1, X.shape[1] - p_w + 1))
+        for i in range(Y.shape[0]):
+            for j in range(Y.shape[1]):
+                if mode == 'max':       # 最大汇聚层
+                    Y[i, j] = X[i: i + p_h, j: j + p_w].max()
+                elif mode == 'avg':     # 平均汇聚层
+                    Y[i, j] = X[i: i + p_h, j: j + p_w].mean()
+        return Y
+    X = torch.tensor([[0.0, 1.0, 2.0], [3.0, 4.0, 5.0], [6.0, 7.0, 8.0]])
+    print(pool2d(X, (2, 2)))
+    
+    # 6.5.2 填充和步幅
+    X = torch.arange(16, dtype=torch.float32).reshape((1, 1, 4, 4))
+    print(X)
+    # 默认情况下，深度学习框架中的步幅与汇聚窗口的大小相同
+    pool2d = nn.MaxPool2d(3)
+    print(pool2d(X))
+    # 手动设置填充和步幅
+    pool2d = nn.MaxPool2d(3, padding=1, stride=2)
+    print(pool2d(X))
+    # 可以设定一个任意大小的矩形汇聚窗口，并分别设定填充和步幅的高度和宽度
+    pool2d = nn.MaxPool2d((2, 3), stride=(2, 3), padding=(0, 1))
+    print(pool2d(X))
 
-
+    # 6.5.3 多个通道
+    # 汇聚层在每个输入通道上单独运算，而不是像卷积层一样在通道上对输入进行汇总
+    X = torch.cat((X, X + 1), 1)
+    print(X)
 
 # ========== 6.6 卷积神经网络（LeNet）  ========== 
 # 包括两部分组成：
@@ -247,5 +282,6 @@ if __name__ == '__main__':
     #v62_ConvolutionsForImages()
     #v63_PaddingAndStride()
     #v64_Multiple_Input_Output_Channel()
-    v66_LeNet()
+    v65_Pooling()
+    #v66_LeNet()
 
