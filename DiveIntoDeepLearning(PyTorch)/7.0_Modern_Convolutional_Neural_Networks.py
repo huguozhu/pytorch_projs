373fb9d0e93de7f81636b0176a1f4ec934e26753
ted.hu <ted.hu@zoom.us>
2024-04-18 18:21:57 +0800

    up

--- DiveIntoDeepLearning(PyTorch)/7.0_Modern_Convolutional_Neural_Networks.py
+++ DiveIntoDeepLearning(PyTorch)/7.0_Modern_Convolutional_Neural_Networks.py
@@ -4,6 +4,7 @@
 import torch
 from torch import nn
 from d2l import torch as d2l
+from torch.nn import functional as F
 
 # ========== 7.1. 深度卷积神经网络（AlexNet）  ========== 
 # Alexnet由八层组成： 5个卷积层 + 两个全连接隐藏层和一个全连接输出层
@@ -112,8 +113,10 @@ def V73_NiN() :
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size, strides, padding),
             nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size=1), nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size=1), nn.ReLU())
+            nn.Conv2d(out_channels, out_channels, kernel_size=1), 
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=1), 
+            nn.ReLU())
     net = nn.Sequential(
         nin_block(1, 96, kernel_size=11, strides=4, padding=0),
         nn.MaxPool2d(3, stride=2),
@@ -143,12 +146,162 @@ def V73_NiN() :
 
 
 
+# ========== 7.4. 含并行连结的网络（GoogLeNet）   ========== 
+def V74_GoogLeNet() :
+    class Inception(nn.Module):
+        # c1--c4是每条路径的输出通道数
+        def __init__(self, in_channels, c1, c2, c3, c4, **kwargs):
+            super(Inception, self).__init__(**kwargs)
+            # 线路1，单1x1卷积层
+            self.p1_1 = nn.Conv2d(in_channels, c1, kernel_size=1)
+            # 线路2，1x1卷积层后接3x3卷积层
+            self.p2_1 = nn.Conv2d(in_channels, c2[0], kernel_size=1)
+            self.p2_2 = nn.Conv2d(c2[0], c2[1], kernel_size=3, padding=1)
+            # 线路3，1x1卷积层后接5x5卷积层
+            self.p3_1 = nn.Conv2d(in_channels, c3[0], kernel_size=1)
+            self.p3_2 = nn.Conv2d(c3[0], c3[1], kernel_size=5, padding=2)
+            # 线路4，3x3最大汇聚层后接1x1卷积层
+            self.p4_1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+            self.p4_2 = nn.Conv2d(in_channels, c4, kernel_size=1)
+
+        def forward(self, x):
+            p1 = F.relu(self.p1_1(x))
+            p2 = F.relu(self.p2_2(F.relu(self.p2_1(x))))
+            p3 = F.relu(self.p3_2(F.relu(self.p3_1(x))))
+            p4 = F.relu(self.p4_2(self.p4_1(x)))
+            # 在通道维度上连结输出
+            return torch.cat((p1, p2, p3, p4), dim=1)
+    
+    # 第一个模块:使用64个通道、7x7卷积层
+    b1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+                   nn.ReLU(),
+                   nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+    # 第二个模块:使用两个卷积层--对应Inception块中的第二条路径
+    b2 = nn.Sequential(nn.Conv2d(64, 64, kernel_size=1),
+                   nn.ReLU(),
+                   nn.Conv2d(64, 192, kernel_size=3, padding=1),
+                   nn.ReLU(),
+                   nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+    # 第三个模块：串联两个完整的Inception块
+    b3 = nn.Sequential(Inception(192, 64, (96, 128), (16, 32), 32),
+                   Inception(256, 128, (128, 192), (32, 96), 64),
+                   nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+    # 第四个模块：串联5个Inception块
+    b4 = nn.Sequential(Inception(480, 192, (96, 208), (16, 48), 64),
+                   Inception(512, 160, (112, 224), (24, 64), 64),
+                   Inception(512, 128, (128, 256), (24, 64), 64),
+                   Inception(512, 112, (144, 288), (32, 64), 64),
+                   Inception(528, 256, (160, 320), (32, 128), 128),
+                   nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+    # 第五个模块：两个Inception块
+    b5 = nn.Sequential(Inception(832, 256, (160, 320), (32, 128), 128),
+                   Inception(832, 384, (192, 384), (48, 128), 128),
+                   nn.AdaptiveAvgPool2d((1,1)),
+                   nn.Flatten())
+    # 组合形成GoogLeNet
+    net = nn.Sequential(b1, b2, b3, b4, b5, nn.Linear(1024, 10))
+
+    # 测试打印整个输入输出过程
+    X = torch.rand(size=(1, 1, 96, 96))
+    for layer in net:
+        X = layer(X)
+        print(layer.__class__.__name__,'output shape:\t', X.shape)
+
+    # 训练
+    lr, num_epochs, batch_size = 0.1, 10, 128
+    train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=96)
+    d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
+
+
+
+
+
+# ========== 7.6. 残差网络（ResNet）   ========== 
+def V76_ResNet() :
+    # 残差块
+    class Residual(nn.Module):  #@save
+        def __init__(self, input_channels, num_channels,
+                    use_1x1conv=False, strides=1):
+            super().__init__()
+            self.conv1 = nn.Conv2d(input_channels, num_channels,
+                                kernel_size=3, padding=1, stride=strides)
+            self.conv2 = nn.Conv2d(num_channels, num_channels,
+                                kernel_size=3, padding=1)
+            if use_1x1conv:
+                self.conv3 = nn.Conv2d(input_channels, num_channels,
+                                    kernel_size=1, stride=strides)
+            else:
+                self.conv3 = None
+            self.bn1 = nn.BatchNorm2d(num_channels)
+            self.bn2 = nn.BatchNorm2d(num_channels)
+
+        def forward(self, X):
+            Y = F.relu(self.bn1(self.conv1(X)))
+            Y = self.bn2(self.conv2(Y))
+            if self.conv3:
+                X = self.conv3(X)
+            Y += X
+            return F.relu(Y)
+
+
+    blk = Residual(3,3)
+    X = torch.rand(4, 3, 6, 6)
+    Y = blk(X)
+    print(Y.shape)
+    blk = Residual(3,6, use_1x1conv=True, strides=2)
+    print(blk(X).shape)
+
+    # ResNet模型
+    def resnet_block(input_channels, num_channels, num_residuals, first_block=False):
+        blk = []
+        for i in range(num_residuals):
+            if i == 0 and not first_block:
+                blk.append(Residual(input_channels, num_channels,
+                                    use_1x1conv=True, strides=2))
+            else:
+                blk.append(Residual(num_channels, num_channels))
+        return blk
+    b1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+    b2 = nn.Sequential(*resnet_block(64, 64, 2, first_block=True))
+    b3 = nn.Sequential(*resnet_block(64, 128, 2))
+    b4 = nn.Sequential(*resnet_block(128, 256, 2))
+    b5 = nn.Sequential(*resnet_block(256, 512, 2))
+    net = nn.Sequential(b1, b2, b3, b4, b5,
+                    nn.AdaptiveAvgPool2d((1,1)),
+                    nn.Flatten(), nn.Linear(512, 10))
+    # 测试： 打印输入输出的宽高
+    X = torch.rand(size=(1, 1, 224, 224))
+    for layer in net:
+        X = layer(X)
+        print(layer.__class__.__name__,'output shape:\t', X.shape)
+
+    # 训练
+    lr, num_epochs, batch_size = 0.05, 10, 256
+    train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=96)
+    d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
+
+
+
+
+# ========== 7.7.稠密连接网络（DenseNet）   ==========
+def V77_DenseNet() :
+    # 定义稠密块
+    def conv_block(input_channels, num_channels):
+        return nn.Sequential(
+            nn.BatchNorm2d(input_channels), nn.ReLU(),
+            nn.Conv2d(input_channels, num_channels, kernel_size=3, padding=1))
+
 
 # ========== main ==========
 if __name__ == '__main__':
     #v71_AlexNet()
     #V72_VGG_11()
-    V73_NiN()
+    #V73_NiN()
+    #V74_GoogLeNet()
+    #V76_ResNet()
+    V77_DenseNet()
 
 
 
